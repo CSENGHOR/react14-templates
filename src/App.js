ef263852d7f78376483e774440b98a2eaf25722f
Keo  Kay <keokay888@gmail.com>
2023-09-09 14:03:38 +0700

    udpate on app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import ProductCard from "./components/ProductCard";
-import { Placeholder } from "react-bootstrap";
-import PlaceholderCard from "./components/PlaceholderCard";
-import NotFoundPage from "./pages/NotFoundPage";
+import { BrowserRouter as BRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
 import Service from "./pages/Service";
-import Footer from "./components/Footer";
-import UserSignUpModal from "./components/UserSignUpModal";
-import HomePage from "./pages/HomePage";
 function App() {
   return (
-    <div className="App">
-      <h1> Hello Everyone! Let's have some fun with reacjs</h1>
-      <HomePage />
-    </div>
+    // BrowserRouter , Routes, Route 
+    <BRouter>
+      <Routes>
+        <Route path="/" index element={<HomePage />} />
+        <Route path="/aboutus" index element={<AboutUs />} />
+        <Route path="/service" index element={<Service />} />
+
+      </Routes>
+
+    </BRouter>
   );
 }
 
