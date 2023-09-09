69bc0e72e8801e35145c8be266801747899ccafd
Keo  Kay <keokay888@gmail.com>
2023-09-09 15:11:23 +0700

    getting data product

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GET_ALL_PRODUCTS } from '../services/productService';
 
 const Products = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    // get product data 
+    GET_ALL_PRODUCTS().then(
+      data => {
+        console.log("Data is : ", data)
+        setProducts(data)
+
+      }
+    ).catch(
+      error => { console.log("Error is : ", error) }
+    )
+  }, []);
   return (
     <div> <h1> All Products </h1></div>
   )
