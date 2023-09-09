d2e039ebb5a538d4c4e73005d8c9078db959d79d
Keo  Kay <keokay888@gmail.com>
2023-09-09 16:03:32 +0700

    adding placeholder

--- src/components/PlaceholderCard.js
+++ src/components/PlaceholderCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/placeholderCardStyle.css";
 const PlaceholderCard = () => {
   return (
-    <div className="placeholder-card card" aria-hidden="true">
+    <div className="placeholder-card card p-0" aria-hidden="true">
       <div className="p-absolute image-pard">
         <img
           src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,34 +1,66 @@
 import React, { useEffect, useState } from 'react'
 import { GET_ALL_PRODUCTS } from '../services/productService';
 import ProductCard from '../components/ProductCard';
+import PlaceholderCard from '../components/PlaceholderCard';
 const Products = () => {
   const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     // get product data 
     GET_ALL_PRODUCTS().then(
       data => {
         console.log("Data is : ", data)
         setProducts(data)
+        // setIsLoading(false)
 
       }
     ).catch(
       error => { console.log("Error is : ", error) }
     )
   }, []);
+
+
+
+  const placeholderCards = (number) => {
+    let placeholders = [];
+    for (var i = 0; i < number; i++) {
+      placeholders.push(
+        <div className="col-3">
+          <PlaceholderCard />
+        </div>
+      )
+    }
+    return placeholders;
+  }
+
+
   return (
     <div className='container '>
       <div className="row ">
         {
-          products.map(
-            product => {
-              return (
-                <div className="col-3">
-                  <ProductCard productData = {product} />
-                </div>
-              )
-            }
-          )
+          isLoading ?
+            (
+              <>
+                {placeholderCards(8)}
+              </>
+
+            ) :
+            <>
+              {
+                products.map(
+                  product => {
+                    return (
+                      <div className="col-3">
+                        <ProductCard productData={product} />
+                      </div>
+                    )
+                  }
+                )
+              }
+            </>
         }
+
+
       </div>
     </div>
   )
