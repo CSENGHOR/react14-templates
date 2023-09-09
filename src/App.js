8b3c3639f6c453b431578ae55fe3697137bccc37
Keo  Kay <keokay888@gmail.com>
2023-09-09 14:19:13 +0700

    update on navbar

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { BrowserRouter as BRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
 import Service from "./pages/Service";
+import NotFoundPage from "./pages/NotFoundPage";
+import CustomNavBar from "./components/CustomNavBar";
 function App() {
   return (
     // BrowserRouter , Routes, Route 
     <BRouter>
+      <CustomNavBar />
       <Routes>
         <Route path="/" index element={<HomePage />} />
-        <Route path="/aboutus" index element={<AboutUs />} />
-        <Route path="/service" index element={<Service />} />
-
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
 
     </BRouter>
---
+++ src/components/CustomNavBar.js
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
+
+function CustomNavBar() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">EarthDX</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+
+                        <NavLink className={"nav-link"} to="/">
+                            Home</NavLink>
+                        <NavLink className={"nav-link"} to="/service">
+                            Service</NavLink>
+                        <NavLink className={"nav-link"} to="/aboutus">
+                            AboutUs</NavLink>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default CustomNavBar;
